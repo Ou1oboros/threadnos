020010ceaba64c08abd31531e4d7223208b544a5
Musab <m.alghamdi.eja@ncgr.gov.sa>
2025-03-16 02:39:42 +0300

    initial api commit

---
+++ src/Threadnos-API/Threadnos-API/Domain/Entities/Page.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Threadnos_API.Domain.Entities
+{
+    public class Page
+    {
+        public Guid Id { get; private set; }
+        public string? Title { get; set; }
+        [Column(TypeName = "timestamp")]
+        public DateTime CreateAt { get; private set; }
+        [Column(TypeName = "timestamp")]
+        public DateTime UpdateAt { get; set; }
+        public bool IsDeleted { get; set; }
+        public Guid ContentId { get; private set; }
+
+    }
+}
---
+++ src/Threadnos-API/Threadnos-API/Infrastructure/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Threadnos_API.Domain.Entities;
+
+namespace PostgreSQL.Data
+{
+    public class AppDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        }
+
+        public DbSet<Threadnos_API.Domain.Entities.Thread> Threads { get; set; }
+        public DbSet<Page> Pages { get; set; }
+        public DbSet<Label> Labels { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
